b40359bec8f9e4d894b0390001276806539368f4
umardevid <umaralfaruq587@gmail.com>
2021-03-24 07:50:00 +0700

    test finishing

--- dist/js/auth.js
+++ dist/js/auth.js
@@ -1,3 +1,19 @@
 // var myModal = new bootstrap.Modal(document.getElementById('loginModal'));
 
 // myModal.toggle();
+
+new Splide("#splide2", {
+    type: "loop",
+    perPage: 1,
+    // perMove: 1,
+    pagination: false,
+    autoWidth: true,
+    // rewind: true,
+    breakpoints: {
+        992: {
+            perPage: 1,
+            autoWidth: false,
+            pagination: true,
+        },
+    },
+}).mount();
